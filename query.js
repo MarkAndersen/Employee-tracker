6eee630aad3b5c9ad2fe40ab52f81f07cbbc8461
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-06-15 14:04:43 -0400

    inquirer now fully functional, now need to add methods to edit DB

--- query.js
+++ query.js
@@ -1,78 +1,79 @@
 //dependencies
-const mysql = require('mysql');
-const inquirer = require('inquirer');
-const cTable = require('console.table');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 console.table([
   {
-    name: 'foo',
-    age: 10
-  }, {
-    name: 'bar',
-    age: 20
-  }
+    name: "foo",
+    age: 10,
+  },
+  {
+    name: "bar",
+    age: 20,
+  },
 ]);
+
 //connection to the db
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
   port: 3306,
-  user: 'root',
-  password: '6488',
-  database: 'employees_db',
+  user: "root",
+  password: "6488",
+  database: "employees_db",
 });
 
+//connection method and initialization call
 connection.connect((err) => {
   if (err) throw err;
   console.log(`connected as id ${connection.threadId}\n`);
   init();
 });
 
+//switch case to guide the user to select an action
 const init = () => {
   inquirer
     .prompt({
-      name: 'action',
-      type: 'list',
-      message: 'What would you like to do?',
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
       choices: [
-        'View all employees',
-        'View all departments',
-        'View all roles',
-        'Add employee',
-        'Add department',
-        'Add role',
-        'Update employee',
-        'Exit',
+        "View all employees",
+        "View all departments",
+        "View all roles",
+        "Add employee",
+        "Add department",
+        "Add role",
+        "Update employee",
+        "Exit"
       ],
     })
     .then((answer) => {
       switch (answer.action) {
-        case 'View all employees':
-          //employee console.table;
+        case "View all employees":
           showEmployee();
           break;
 
-        case 'View all departments':
-          //employee where department console.table
+        case "View all departments":
           showEmployeeDept();
           break;
 
-        case 'View all roles':
-          //employee where role = console.table
+        case "View all roles":
           showEmployeeRole();
           break;
 
-        case 'Add employee':
+        case "Add employee":
           addEmployee();
           break;
 
-        case 'Add department':
+        case "Add department":
           addDepartment();
           break;
 
-        case 'Add role':
+        case "Add role":
           addRole();
           break;
 
-        case 'Exit':
+        case "Exit":
           connection.end();
           break;
 
@@ -80,44 +81,128 @@ const init = () => {
           console.log(`invalid action: ${answer.action}`);
           break;
       }
-      //switch case to guide the user to select an action
+      
     });
 };
+
+//prompts user if they want to continue or quit.
+const nextStep = () => {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "confirm",
+      message: "Would you like to edit more?",
+    })
+    .then((answer) => {
+      if (answer.action === true) {
+        init();
+      } else {
+        connection.end();
+      }
+    });
+};
+
+//Methods to view database
 const showEmployee = () => {
-  const query = 'SELECT * FROM employee';
+  const query = "SELECT * FROM employee";
   connection.query(query, (err, res) => {
     if (err) throw err;
-   console.table (res);
+    console.table(res);
+    nextStep();
   });
-  connection.end();
 };
-  const showEmployeeDept = () => {
-    const query = 'SELECT * FROM department';
-    connection.query(query, (err, res) => {
-      if (err) throw err;
-     console.table (res);
-    });
-    connection.end();
-  };
-  const showEmployeeRole = () => {
-    const query = 'SELECT * FROM role';
-    connection.query(query, (err, res) => {
-      if (err) throw err;
-     console.table (res);
-    });
-    connection.end();
-  };
-  const addEmployee = () => {
+const showEmployeeDept = () => {
+  const query = "SELECT * FROM department";
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    nextStep();
+  });
+};
+const showEmployeeRole = () => {
+  const query = "SELECT * FROM role";
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    nextStep();
+  });
+};
+
+//methods to add employees, departments, roles, etc.
 //addEmployee(inquirer - input, INSERT INTO)
-  };
+const addEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        name: "first",
+        type: "input",
+        message: "First Name",
+      },
+      {
+        name: "last",
+        type: "input",
+        message: "Last Name",
+      },
+      {
+        name: "manager",
+        type: "input",
+        message: "Manager ID",
+      },
+      {
+        name: "role",
+        type: "input",
+        message: "Role ID",
+      },
+    ])
+    .then((answer) => {
+      console.log(answer);
+      nextStep();
+    });
 
-  const addDepartment = () => {
-//addDepartment(inquirer - input, INSERT INTO)
-  };
+};
 
-  const addRole = () => {
-//addRole(inquirer - input, INSERT INTO)
-  };
+const addDepartment = () => {
+  //addDepartment(inquirer - input, INSERT INTO)
+  inquirer
+  .prompt(
+    {
+      name: "dept",
+      type: "input",
+      message: "Department Name",
+    }
+  )
+  .then((answer) => {
+    console.log(answer);
+    nextStep();
+  });
+
+};
+
+const addRole = () => {
+  //addRole(inquirer - input, INSERT INTO)
+  inquirer
+  .prompt([
+    {
+      name: "role",
+      type: "input",
+      message: "Role",
+    },
+    {
+      name: "salary",
+      type: "input",
+      message: "Salary",
+    },
+    {
+      name: "dept",
+      type: "input",
+      message: "Department ID",
+    }
+  ])
+  .then((answer) => {
+    console.log(answer);
+    nextStep();
+  });
+};
 
 //TODO:
 //INIT();
